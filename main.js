aabbb6e6ebe4f9618a36c5f894206378d1b10680
satvik-datta <73769667+satvik-datta@users.noreply.github.com>
2021-04-07 21:15:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("My first console project");
+var name1="Satvik";
+console.log(name1);
+var name2="Datta Indrakanti"
+console.log(name2);
+var full_name1=name1.concat(name2);
+console.log(full_name1);
+var number1="29";
+var number2="33";
+var add1=number1+number2;
+console.log(add1);
+var subtract1=number1-number2;
+console.log(subtract1);
+var multiply1=number1*number2;
+console.log(multiply1);
+var divide1=number1/number2;
+console.log(divide1);
